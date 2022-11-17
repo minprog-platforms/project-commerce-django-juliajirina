d1aef78673f4023819c33eca02ea0839ae87d321
juliajirina <julianageisler@gmail.com>
2022-11-17 13:50:49 +0100

    Add listing.html and created models in models.py

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,25 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    price = models.PositiveIntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.id}.{self.title}, {self.description}: {self.price}"
+
+class Bidding(models.Model):
+    list_name = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="all_listings")
+    bid = models.PositiveIntegerField
+
+    def __str__(self) -> str:
+        return f"{self.list_name}: {self.bid}"
+
+class Comments(models.Model):
+    username = models.CharField(max_length=64) #username
+    comment = models.TextField
+
+    def __str__(self) -> str:
+        return f"{self.username}: {self.comment}"
